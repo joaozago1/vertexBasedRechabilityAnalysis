cd2aaf285c74180a2b549577515afff3ed7d4604
Joao Gabriel Zago <joao@Joaos-MacBook-Pro.local>
2023-06-26 11:31:30 -0300

    Updating network mapping for the new orthant intersection procedure

--- src/vertexBasedRechabilityAnalysis.jl
+++ src/vertexBasedRechabilityAnalysis.jl
@@ -13,7 +13,7 @@ module vertexBasedRechabilityAnalysis
 
     include("utils/vertexOperations.jl")
 
-    export identify_adjascent_vertices, identifying_orthant_intersection_points, filtering_zeros, convert_to_matrix, convert_to_vector, identify_non_vertices
+    export identify_adjascent_vertices, identifying_orthant_intersection_points, comput_internal_intersections, compute_intersections, filtering_zeros, convert_to_matrix, convert_to_vector, identify_non_vertices
 
     include("utils/origin.jl")
 
@@ -22,200 +22,170 @@ module vertexBasedRechabilityAnalysis
     export network_mapping, network_mapping2, network_mapping3
 
     function network_mapping(P_cp, neural_network)
-
+    
+        input_dimensionality = size(P_cp)[1];
+    
         for i in 1:length(neural_network)-1
-
+    
             P_hat = affine_mapping(P_cp, params(neural_network[i])[1], params(neural_network[i])[2]);
-            adj_vertices = identify_adjascent_vertices(P_hat);
-
-            intersection_index_min = size(P_hat)[2] + 1;
-            P_hat = identifying_orthant_intersection_points(P_hat, adj_vertices)
-            intersection_index_max = size(P_hat)[2];
-            P_hat_aux = origin_search(P_hat, intersection_index_min, intersection_index_max)
-        
-            if P_hat_aux != nothing
-
-                P_hat = [P_hat P_hat_aux];
-
-            end;
-            
+            P_hat = compute_intersections(P_hat, input_dimensionality)
+    
             P_hat = round.(P_hat, digits=6);
-
+    
             P_cp = filtering_zeros(P_hat);
             P_cp = identify_non_vertices(P_cp);
-
+    
         end
-
+    
         P_hat = affine_mapping(P_cp, params(neural_network[length(neural_network)])[1], params(neural_network[length(neural_network)])[2]);
-
+    
         return P_hat
-
+    
     end;
 
     function network_mapping2(P_cp, neural_network)
-
+    
+        input_dimensionality = size(P_cp)[1];
+    
         P_input = [P_cp]
-
+    
         for j in 1:length(neural_network)
-        
-            input_dims = size(params(neural_network.layers[j])[1])[2];
-            output_dims = size(params(neural_network.layers[j])[1])[1];
-
+    
             P_hat_div_aux = []
-
+    
             for i in 1:size(P_input)[1]
-
+    
                 if !isempty(P_input[i])
-
+    
                     P_hat = copy(P_input[i])
-
+    
                     if j > 1
-
+    
                         P_hat = filtering_zeros(P_hat)
-                        
+    
                         if length(size(P_hat)) > 1
-
+    
                             P_hat = identify_non_vertices(P_hat)
-
+    
                         end
-
+    
                     end
-
+    
                     P_hat = affine_mapping(P_hat, params(neural_network[j])[1], params(neural_network[j])[2])
-
+    
                     if j < length(neural_network)
-                    
+    
                         if length(size(P_hat)) > 1
-
-                            adj_vertices = identify_adjascent_vertices(P_hat)
-                            intersection_index_min = size(P_hat)[2] + 1;
-                            P_hat = identifying_orthant_intersection_points(P_hat, adj_vertices)
-                            intersection_index_max = size(P_hat)[2];
-                            P_hat_aux = origin_search(P_hat, intersection_index_min, intersection_index_max)
-                        
-                            if P_hat_aux != nothing
-
-                                P_hat = [P_hat P_hat_aux];
-
-                            end;
                             
+                            P_hat = compute_intersections(P_hat, input_dimensionality)
                             P_hat = round.(P_hat, digits=6);
-                        
+    
                         end
-                        
+    
                         P_hat = get_points_per_orthant(P_hat);
                         P_hat = remove_empty_orthants(P_hat);
-
+    
                         if isempty(P_hat_div_aux)
-
+    
                             P_hat_div_aux = P_hat
-
+    
                         else
-
+    
                             append!(P_hat_div_aux, P_hat)
-
+    
                         end
-
+    
                     else
-
+    
                         push!(P_hat_div_aux, P_hat)
-
+    
                     end
-
+    
                 end
-
+    
             end
-
+    
             P_input = copy(P_hat_div_aux)
-
+    
         end
-
+    
         return P_input 
-
+    
     end;
 
     function network_mapping3(P_cp, neural_network, divs)
-
+    
+        input_dimensionality = size(P_cp)[1];
+    
         P_input = [P_cp]
-
+    
         for j in 1:length(neural_network)
-
+    
             input_dims = size(params(neural_network.layers[j])[1])[2];
             output_dims = size(params(neural_network.layers[j])[1])[1];
-
+    
             P_hat_div_aux = []
-
+    
             for i in 1:size(P_input)[1]
-
+    
                 if !isempty(P_input[i])
-
+    
                     P_hat = copy(P_input[i])
-
+    
                     if j > 1
-
+    
                         P_hat = filtering_zeros(P_hat)
-
+    
                         if length(size(P_hat)) > 1
-
+    
                             P_hat = identify_non_vertices(P_hat)
-
+    
                         end
-
+    
                     end
-
+    
                     P_hat = affine_mapping(P_hat, params(neural_network[j])[1], params(neural_network[j])[2])
-
+    
                     if j < length(neural_network)
-
+    
                         if length(size(P_hat)) > 1
-
-                            adj_vertices = identify_adjascent_vertices(P_hat)
-                            intersection_index_min = size(P_hat)[2] + 1;
-                            P_hat = identifying_orthant_intersection_points(P_hat, adj_vertices)
-                            intersection_index_max = size(P_hat)[2];
-                            P_hat_aux = origin_search(P_hat, intersection_index_min, intersection_index_max)
-                        
-                            if P_hat_aux != nothing
-
-                                P_hat = [P_hat P_hat_aux];
-
-                            end;
-                            
+    
+                            P_hat = compute_intersections(P_hat, input_dimensionality);
                             P_hat = round.(P_hat, digits=6);
-
+    
                         end
-
+    
                         P_hat = get_points_per_orthant(P_hat);
                         P_hat = remove_empty_orthants(P_hat);
                         P_hat = merging_sets(P_hat, divs);
-
+    
                         if isempty(P_hat_div_aux)
-
+    
                             P_hat_div_aux = P_hat
-
+    
                         else
-
+    
                             append!(P_hat_div_aux, P_hat)
-
+    
                         end
-
-
+    
+    
                     else
-
+    
                         push!(P_hat_div_aux, P_hat)
-
+    
                     end
-
+    
                 end
-
+    
             end
-
+    
             P_input = copy(P_hat_div_aux)
-
+    
         end
-
+    
         return P_input 
-
+    
     end; 
     
 end;
